45639fe9c5f40a67c9fcaf5b4e0a1129628018b0
JPSO18 <joao.paulojp18@hotmail.com>
2018-01-16 15:12:41 -0200

    Profile update now handles only one or more changes

--- apps/api/lib/api/controllers/profile_controller.ex
+++ apps/api/lib/api/controllers/profile_controller.ex
@@ -21,11 +21,19 @@ defmodule Api.ProfileController do
         end
     end
 
-    def update_profile(conn, %{"id" => profile_id,
-              "email" => email,
-              "phone" => phone,
-              "birth_date" => birth_date}) do
-        map_changes = %{email: email, phone: phone, birth_date: birth_date}
+
+    def update_profile(conn, params) do
+
+        var = check_params(params)
+        {profile_id, phone_1, email_1, birth_date_1} = var
+
+        email_fn = check(profile_id, email_1, :email)
+        phone_fn = check(profile_id, phone_1, :phone)
+        birth_date_fn = check(profile_id, birth_date_1, :birth_date)
+
+
+
+        map_changes = %{email: email_fn, phone: phone_fn, birth_date: birth_date_fn}
         {:ok, profile} = Profile.update(profile_id, map_changes)
         render(conn, "updated_profile.json", %{id: profile.id,
                                                email: profile.email,
@@ -33,6 +41,60 @@ defmodule Api.ProfileController do
                                                birth_date: profile.birth_date})
 
     end
+
+    defp check(profile_id, param, field)
+    defp check(profile_id, param, :email) do
+        case param do
+            "" ->
+                {:ok, profile} = do_get_profile(profile_id)
+                profile.email
+            value -> value
+        end
+    end
+
+    defp check(profile_id, param, :phone) do
+        case param do
+            "" ->
+                {:ok, profile} = do_get_profile(profile_id)
+                profile.phone
+            value -> value
+        end
+    end
+
+    defp check(profile_id, param, :birth_date) do
+        case param do
+            "" ->
+                {:ok, profile} = do_get_profile(profile_id)
+                profile.birth_date
+            value -> value
+        end
+    end
+
+    defp check_params(params) do
+        profile_id =
+            params |> Map.get("id")
+
+        phone =
+            if params |> Map.has_key?("phone") do
+                params |> Map.get("phone")
+            else
+                ""
+            end
+        email =
+            if params |> Map.has_key?("email") do
+                params |> Map.get("email")
+            else
+                ""
+            end
+        birth_date =
+            if params |> Map.has_key?("birth_date") do
+                params |> Map.get("birth_date")
+            else
+                ""
+            end
+        {profile_id, phone, email, birth_date}
+    end
+
 
     def update_nickname(conn, %{"profile_id" => profile_id,
                                 "nickname" => nickname}) do
@@ -58,6 +120,7 @@ defmodule Api.ProfileController do
     end
 
     def get_nickname(conn, %{"id" => profile_id}) do
+
         case do_get_profile(profile_id, :nickname) do
             {:ok, nickname} -> render(conn, "nickname.json", %{nicknames: nickname})
             {:error, message, code} ->
